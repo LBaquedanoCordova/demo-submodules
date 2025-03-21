2307f18858ae94299ad561b72fa93110b618a872
Luis Baquedano <lfbcordova@gmail.com>
2025-03-21 06:28:50 -0500

    Agregar clase Person en script

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log('Inicializar script');
+
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+const juan = new Person('Juan', 15);
